43421477a44c45a8b590beaf1c6bb491812170d4
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2024-01-01 15:49:51 +0100

    add button & format time correctly

--- popup.js
+++ popup.js
@@ -1,32 +1,44 @@
+// add clear timer button to page
+const button = document.createElement('button');
+button.innerText = 'Clear Timer';
+button.addEventListener('click', () => {
+    alert('Timer is now cleared');
+    chrome.storage.sync.set({ totalTime: 0 })
+});
+document.body.appendChild(button)
+
 document.addEventListener('DOMContentLoaded', function () {
     chrome.storage.sync.get(['totalTime'], function (result) {
         if (chrome.runtime.lastError) {
             console.error(chrome.runtime.lastError);
         } else {
             let totalTime = result.totalTime || 0;
-            console.log('DOM Content loaded')
-            console.log('Total time retrieved:', totalTime);
             document.getElementById('timeSpent').innerText = `Total time spent on YouTube: ${formatTime(totalTime)}`;
         }
     });
 });
 
-const pauseTimer = document.getElementById("pauseTimer");
-if (pauseTimer) {
-    pauseTimer.onclick = function () {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+function formatTime(milliseconds) {
+    if (milliseconds == 0) {
+        return "00:00:00";
+    }
+    if (milliseconds < 0) {
+        return "Invalid time";
+    }
 
-        });
-    };
-}
+    let seconds = Math.floor(milliseconds / 1000);
 
-function formatTime(seconds) {
     const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const secs = seconds % 60;
-    return `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
+    seconds %= 3600;
+    const minutes = Math.floor(seconds / 60);
+    seconds %= 60;
+
+    // format time HH:MM:SS
+    const formattedTime = `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+    return formattedTime;
 }
 
-function pad(num) {
-    return num.toString().padStart(2, '0');
+// helper function to pad zero for single-digit numbers
+function padZero(number) {
+    return (number < 10) ? `0${number}` : number;
 }
