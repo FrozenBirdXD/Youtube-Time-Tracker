17d71d780ca21129b282974175c9912be3324fc5
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2024-01-01 15:49:14 +0100

    move from content to background script

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
-// Function to check and handle the current tab
+const STORAGE_KEY_START_TIME = "startTime";
+const STORAGE_KEY_TOTAL_TIME = "totalTime";
+let isTracking = false; // track whether YouTube is currently being tracked
+
 function checkCurrentTab() {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         if (tabs && tabs[0]) {
@@ -6,40 +9,41 @@ function checkCurrentTab() {
             const url = currentTab.url;
             console.log("URL: ", url);
 
-            if (url && typeof url === 'string' && url.startsWith('https://www.youtube.com')) {
-                // The current tab is on YouTube
+            const youtubeRegex = /^https:\/\/www\.youtube\.com/;
 
-                console.log("start tracking-background");
-                chrome.tabs.sendMessage(currentTab.id, { type: 'startTracking' });
+            if (url && typeof url === 'string' && youtubeRegex.test(url)) { // if current tab is on YouTube
+                // start tracking if not already tracking
+                if (!isTracking) {
+                    const startTime = new Date().getTime();
+                    chrome.storage.sync.set({ [STORAGE_KEY_START_TIME]: startTime });
+                    isTracking = true;
+                }
 
-            } else if (url && typeof url === 'string') {
-                console.log("stop tracking-background");
-                chrome.tabs.sendMessage(currentTab.id, { type: 'stopTracking' });
+            } else if (url && typeof url === 'string') { // The current tab is not on YouTube
+                // stop tracking if currently tracking
+                if (isTracking) {
+                    const endTime = new Date().getTime();
+
+                    chrome.storage.sync.get([STORAGE_KEY_START_TIME, STORAGE_KEY_TOTAL_TIME], function (result) {
+                        const startTime = result[STORAGE_KEY_START_TIME];
+                        const duration = endTime - startTime;
+
+                        const savedTotalTime = result[STORAGE_KEY_TOTAL_TIME] || 0;
+                        chrome.storage.sync.set({ [STORAGE_KEY_TOTAL_TIME]: savedTotalTime + duration });
+                    });
+
+                    isTracking = false;
+                }
             }
         }
     });
-}
+};
 
-// Event listener for tab changes
+// event listener for tab changes
 chrome.tabs.onActivated.addListener(function (activeInfo) {
-    // Check the current tab when the tab is changed
+    // check the current tab when the tab is changed
     checkCurrentTab();
 });
 
-// Initial check when the extension is loaded
+// initial check when the extension is loaded
 checkCurrentTab();
-
-// chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//     const tabId = tabs[0].id;
-//     chrome.tabs.sendMessage(tabId, { msg: 'checkInjection' }, function (response) {
-//         if (response === true) {
-//             console.log('Content script is already injected');
-//         } else {
-//             console.log('Content script is not injected, injecting now');
-//             chrome.scripting.executeScript({
-//                 target: { tabId: tabId },
-//                 files: ["content.js"]
-//             });
-//         }
-//     });
-// });
