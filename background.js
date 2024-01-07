39d7a9f1011857fd35edea560cab1108ad121181
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2024-01-07 17:17:03 +0100

    tried fixing bug but failed

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
 const STORAGE_KEY_START_TIME = "startTime";
 const STORAGE_KEY_TOTAL_TIME = "totalTime";
 let isTracking = false; // track whether YouTube is currently being tracked
+let isBrowserWindowFocused = true;
 
-function checkCurrentTab() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+async function checkCurrentTab() {
+    chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
         if (tabs && tabs[0]) {
             const currentTab = tabs[0];
             const url = currentTab.url;
@@ -12,42 +13,55 @@ function checkCurrentTab() {
             const youtubeRegex = /^https:\/\/www\.youtube\.com/;
 
             if (url && typeof url === 'string' && youtubeRegex.test(url)) { // if current tab is on YouTube
+                console.log("is on youtube");
                 if (!isTracking) {
-                    startTracking();
+                    await startTracking();
                 }
             } else if (url && typeof url === 'string') { // the current tab is not on YouTube
                 if (isTracking) {
-                    stopTracking();
+                    await stopTracking();
                 }
             }
         }
     });
 };
 
-function startTracking() {
-    // start tracking if not already tracking
-    chrome.storage.sync.get([STORAGE_KEY_START_TIME]).then((result) => {
-        const savedStartTime = result[STORAGE_KEY_START_TIME];
-        const startTime = new Date().getTime() / 1000;
-
-        if (savedStartTime == null || savedStartTime == 0) {
-            chrome.storage.sync.set({ [STORAGE_KEY_START_TIME]: startTime });
-        }
+async function startTracking() {
+    const result = await new Promise((resolve) => {
+        chrome.storage.sync.get([STORAGE_KEY_START_TIME], resolve);
     });
+    const savedStartTime = result[STORAGE_KEY_START_TIME];
+    const startTime = Math.floor(new Date().getTime() / 1000);
+
+    if (savedStartTime == null || savedStartTime == 0) {
+        await new Promise((resolve) => {
+            chrome.storage.sync.set({ [STORAGE_KEY_START_TIME]: startTime }, resolve);
+        });
+    }
 
     isTracking = true;
 }
 
-function stopTracking() {
-    const endTime = new Date().getTime() / 1000;
+async function stopTracking() {
+    const endTime = Math.floor(new Date().getTime() / 1000);
+    const result = await new Promise((resolve) => {
+        chrome.storage.sync.get([STORAGE_KEY_START_TIME, STORAGE_KEY_TOTAL_TIME], resolve);
+    });
+    const startTime = result[STORAGE_KEY_START_TIME];
 
-    chrome.storage.sync.get([STORAGE_KEY_START_TIME, STORAGE_KEY_TOTAL_TIME], function (result) {
-        const startTime = result[STORAGE_KEY_START_TIME];
-        const duration = endTime - startTime;
-        const savedTotalTime = result[STORAGE_KEY_TOTAL_TIME] || 0;
+    if (startTime == 0) {
+        return;
+    }
 
-        chrome.storage.sync.set({ [STORAGE_KEY_TOTAL_TIME]: savedTotalTime + duration });
-        chrome.storage.sync.set({ [STORAGE_KEY_START_TIME]: 0 });
+    const duration = endTime - startTime;
+    const savedTotalTime = result[STORAGE_KEY_TOTAL_TIME] || 0;
+
+    await new Promise((resolve) => {
+        chrome.storage.sync.set({ [STORAGE_KEY_TOTAL_TIME]: savedTotalTime + duration }, resolve);
+    });
+
+    await new Promise((resolve) => {
+        chrome.storage.sync.set({ [STORAGE_KEY_START_TIME]: 0 }, resolve);
     });
 
     isTracking = false;
@@ -60,37 +74,44 @@ const ALARM_NAME = '15sec';
 async function createAlarm() {
     const alarm = await chrome.alarms.get(ALARM_NAME);
     if (typeof alarm === 'undefined') {
+        console.log('created alarm');
         chrome.alarms.create(ALARM_NAME, {
             periodInMinutes: 0.25
         });
+        console.log('check tab from alarm creation');
         checkCurrentTab();
     }
 }
 
 // check every 15 seconds
-chrome.alarms.onAlarm.addListener(() => {
+chrome.alarms.onAlarm.addListener(async () => {
     console.log('alarm');
-    checkCurrentTab();
+    await checkCurrentTab();
 });
 
-chrome.tabs.onUpdated.addListener(() => {
+chrome.tabs.onUpdated.addListener(async () => {
     console.log('updated');
     // check the current tab when the tab is changed
-    checkCurrentTab();
+    await checkCurrentTab();
 });
 
-chrome.tabs.onActivated.addListener(() => {
+chrome.tabs.onActivated.addListener(async () => {
     console.log('activated');
-    checkCurrentTab();
+    await checkCurrentTab();
 });
 
-chrome.windows.onFocusChanged.addListener((window) => {
+chrome.windows.onFocusChanged.addListener(async (window) => {
+    // if chrome has no more focused windows, also happens when the user clicks the extension icon
     if (window == chrome.windows.WINDOW_ID_NONE) {
-        console.log('window lost');
-        // wtf
-        // stopTracking();
+        if (isTracking && isBrowserWindowFocused) {
+            console.log('window lost from youtube');
+            await stopTracking();
+            isBrowserWindowFocused = false;
+        }
     } else {
-        // checkCurrentTab();
+        console.log("Ok we are back");
+        isBrowserWindowFocused = true;
+        await checkCurrentTab();
     }
 });
 
